37d1a556f3ba19e9043f14d27544c946a39bfc9d
Bayu <rizqi.bayu@ui.ac.id>
2025-03-26 11:51:03 +0700

    init repo

---
+++ classSubscription/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ClasssubscriptionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'classSubscription'
---
+++ forumManagement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ForummanagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'forumManagement'
